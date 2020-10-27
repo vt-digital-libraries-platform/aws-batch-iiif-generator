fc08b55361398bc3b49a30326f2b5bc5b201f209
Yinlin Chen <ylchen@vt.edu>
2020-10-27 14:56:42 -0400

    create one-click deploy button

--- README.md
+++ README.md
@@ -14,26 +14,68 @@
 2. Generate tiles and manifests
 3. Upload to target S3 bucket
 
-## Batch
-* Compute environment: [computeenv.json](examples/computeenv.json)
-* Job Definition: [jobdefinition.json](examples/jobdefinition.json)
-* Job Queue: [jobqueue.json](examples/jobqueue.json)
+### Deploy aws-batch-iiif-generator using CloudFormation stack
+#### Step 1: Launch CloudFormation stack
+[![Launch Stack](https://cdn.rawgit.com/buildkite/cloudformation-launch-stack-button-svg/master/launch-stack.svg)](https://console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/new?&templateURL=https://vtdlp-dev-cf.s3.amazonaws.com/awsiiifs3batch.template)
+
+Click *Next* to continue
+
+#### Step 2: Specify stack details
+
+| Name | Description |
+|----------|:-------------:|
+| Stack name | any valid name |
+| BatchRepositoryName | any valid name for Batch process repository |
+| DockerImage | any valid Docker image. E.g. yinlinchen/vtl:iiifs3_v3 |
+| JDName | any valid name for Job definition |
+| JQName | any valid name for Job queue |
+| LambdaFunctionName | any valid name for Lambda function |
+| LambdaRoleName | any valid name for Lambda role  |
+| S3BucketName | any valid name for S3 bucket |
+
+#### Step 3: Configure stack options
+Leave it as is and click **Next**
+
+#### Step 4: Review
+Make sure all checkboxes under Capabilities section are **CHECKED**
+
+Click *Create stack*
+
+### Deploy aws-batch-iiif-generator using AWS CLI
+
+Run the following in your shell to deploy the application to AWS:
+```bash
+aws cloudformation create-stack --stack-name awsiiifs3batch --template-body file://awsiiifs3batch.template --capabilities CAPABILITY_NAMED_IAM
+```
+
+See [Cloudformation: create stack](https://docs.aws.amazon.com/cli/latest/reference/cloudformation/create-stack.html) for `--parameters` option
+
+### Usage
+* Prepare [task.json](examples/task.json) 
+* Upload [task.json](examples/task.json) to the S3 bucket created after the deployment.
+* Go to `AWS_BUCKET_NAME` to see the end results for generated IIIF tiles and manifests.
+
+### Cleanup
+
+To delete the sample application that you created, use the AWS CLI. Assuming you used your project name for the stack name, you can run the following:
+
+```bash
+aws cloudformation delete-stack --stack-name stackname
+```
+
+## Batch Configuration
+* Compute Environment: Type: `EC2`, MinvCpus: `0`, MaxvCpus: `128`, InstanceTypes: `optimal`
+* Job Definition: Type: `container`, Image: `DockerImage`, Vcpus: `2`, Memory: `2000`
+* Job Queue: Priority: `10`
 
 ## S3
 * SRC_BUCKET: For raw images and CSV files to be processed
 	* Raw image files
 	* CSV files
 * AWS_BUCKET_NAME: For saving tiles and manifests files
-* Batch_Bucket: For task files
 
-## Lambda
-* [lambda-example.py](examples/lambda-example.py): Submit a batch job when a task file is upload to a S3 bucket
-	* Add S3 trigger: 
-		* Event type: ObjectCreated
-
-## IAM
-* lambda_function_role: Append Read access to S3 buckets and permission to submit a batch job policy
-* ecsInstanceRole: Append Read/Write access to S3 buckets policy
+## Lambda function
+* [index.py](src/index.py): Submit a batch job when a task file is upload to a S3 bucket
 
 ## Task File
 * example: [task.json](examples/task.json)
@@ -54,5 +96,5 @@
 ```
 
 ## IIIF S3 Docker image
-* [iiif_s3_docker](docker)
-* Image at Docker Hub: [yinlinchen/vtl:iiifs3batch](https://cloud.docker.com/repository/docker/yinlinchen/vtl/tags)
+* [iiif_s3_docker](https://github.com/vt-digital-libraries-platform/iiif_s3_docker)
+* Image at Docker Hub: [yinlinchen/vtl:iiifs3_v3](https://cloud.docker.com/repository/docker/yinlinchen/vtl/tags)
--- src/index.py
+++ src/index.py
@@ -5,8 +5,8 @@ import argparse
 import os
 import sys
 import time
-from datetime import datetime
 
+from datetime import datetime
 from botocore.compat import total_seconds
 
 print('Loading function')
@@ -14,19 +14,25 @@ print('Loading function')
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
+    inputFileName = ""
+    bucketName = ""
 
-    # Get the object from the event and show its content type
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    for record in event['Records']:
+      bucketName = record['s3']['bucket']['name']
+      inputFileName = record['s3']['object']['key']
+
+    print(bucketName)
+    print(inputFileName)
+
     try:
-        response = s3.get_object(Bucket=bucket, Key=key)
+        response = s3.get_object(Bucket=bucketName, Key=inputFileName)
         print("CONTENT TYPE: " + response['ContentType'])
         filecontent = response['Body'].read().decode('utf-8')
         content = json.loads(filecontent)
         for envs in content['environment']:
             if envs['name'] == 'AWS_REGION':
                 region_name = envs['value']
-        
+
         batch = boto3.client(
             service_name='batch',
             region_name=region_name,
@@ -36,10 +42,10 @@ def lambda_handler(event, context):
             service_name='logs',
             region_name=region_name,
             endpoint_url='https://logs.'+ region_name +'.amazonaws.com')
-    
+
         spin = ['-', '/', '|', '\\', '-', '/', '|', '\\']
         logGroupName = '/aws/batch/job'
-    
+
         jobName = content['jobName']
         jobQueue = content['jobQueue']
         jobDefinition = content['jobDefinition']
@@ -54,14 +60,14 @@ def lambda_handler(event, context):
             jobQueue=jobQueue,
             jobDefinition=jobDefinition,
             containerOverrides={
-                'command': command, 
+                'command': command,
                 'environment': envlist
             }
         )
 
         jobId = submitJobResponse['jobId']
         print ('Submitted job [%s - %s] to the job queue [%s]' % (jobName, jobId, jobQueue))
-    
+
         spinner = 0
         running = False
         startTime = 0
@@ -85,12 +91,13 @@ def lambda_handler(event, context):
             else:
                 print ('\rJob [%s - %s] is %-9s... %s' % (jobName, jobId, status, spin[spinner % len(spin)]), sys.stdout.flush())
                 spinner += 1
-        
+
         return response['ContentType']
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
+
 
 def printLogs(logGroupName, logStreamName, startTime):
     kwargs = {'logGroupName': logGroupName,
@@ -138,10 +145,6 @@ def createEnvList(content):
             value = env['value']
             region = {'name': 'AWS_REGION', 'value': value}
             envlist.append(region)
-        elif env['name'] == 'COLLECTION_NAME':
-            value = env['value']
-            collection_name = {'name': 'COLLECTION_NAME', 'value': value}
-            envlist.append(collection_name)
         elif env['name'] == 'UPLOAD_BOOL':
             value = env['value']
             upload = {'name': 'UPLOAD_BOOL', 'value': value}
@@ -170,17 +173,9 @@ def createEnvList(content):
             value = env['value']
             dest_bucket = {'name': 'DEST_BUCKET', 'value': value}
             envlist.append(dest_bucket)
-        elif env['name'] == 'DEST_PREFIX':
-            value = env['value']
-            dest_prefix = {'name': 'DEST_PREFIX', 'value': value}
-            envlist.append(dest_prefix)
         elif env['name'] == 'DEST_URL':
             value = env['value']
             dest_url = {'name': 'DEST_URL', 'value': value}
             envlist.append(dest_url)
-        elif env['name'] == 'DIR_PREFIX':
-            value = env['value']
-            dir_prefix = {'name': 'DIR_PREFIX', 'value': value}
-            envlist.append(dir_prefix)
 
     return envlist
